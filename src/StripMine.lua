2c1015bd92c45c71cdedc396fdcd9fd0bec47eae
Jonathan Poalses <7905544+PerpetuaLux@users.noreply.github.com>
2021-06-07 13:21:42 +0100

    fixed a thing where it wasn't actual putting any code in a new file

--- src/StripMine.lua
+++ src/StripMine.lua
@@ -2,7 +2,6 @@
 --- This will be an advanced implementation of Strip3 made in my IDE rather than in-game
 ---
 
-
 --- First declare variables
 maxStrips = 0 --- Maximum number of strips, should be multiple of 2 for efficiency, 0 for unlimited
 stripLength = 32 --- Length of strips, 32 default, don't go too high or chunks may not be loaded
